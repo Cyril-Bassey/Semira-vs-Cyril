1320918412971bcea894b858f1069efbd87c3152
Cyril-Bassey <cyrilbassey4@gmail.com>
2024-07-31 16:56:29 +0100

    Second Commit with some folder structures

---
+++ src/Components/Pages/HomePage/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='w-full bg-brown h-[80px] top-0'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
