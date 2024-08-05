e8ab439cfdb9ef822706f46040a2127a64db2ae8
Cyril-Bassey <cyrilbassey4@gmail.com>
2024-08-05 21:39:19 +0100

    updates

--- src/Components/Pages/HomePage/Hero.jsx
+++ src/Components/Pages/HomePage/Hero.jsx
@@ -1,37 +1,36 @@
-import React from 'react'
-import Bubble from '../../../images/bubbles-png-11414.png'
-import Popover from '../../../images/bubbles-png-11420.png'
+import React from 'react';
+import Bubble from '../../../images/bubbles-png-11414.png';
+import Popover from '../../../images/bubbles-png-11420.png';
 
 const Hero = () => {
     return (
-        <div className='h-screen w-screen bg-[#F5F5F5] flex justify-between'>
-            <div className='w-[20%] h-[100%]'>
-                <img src={Bubble} className='object-cover bubble-animation' alt="" />
+        <div className='h-screen w-full bg-[#F5F5F5] flex flex-col md:flex-row justify-between items-center p-4 md:p-0'>
+            <div className='lg:w-[25%] lg:h-[100%]  hidden md:w-[20%] md:flex justify-center md:justify-start'>
+                <img src={Bubble} className='object-cover bubble-animation  md:w-auto ' alt="" />
             </div>
-            <div className=' w-[50%] h-[100%] content-center block items-center'>
-               
-                <p className='text-xl text-[#FF6600]  flex justify-center font-bold'>
+            <div className='w-[50%]  md:w-[50%] text-center md:text-left my-4 md:my-0 border border-red-800'>
+                <p className='text-lg md:text-xl text-[#FF6600] text-center font-bold'>
                     B2B LEAD GENERATION AGENCY
                 </p>
-                <h1 className='text-7xl font-bold text-center mt-5  '>
+                <h1 className='text-4xl text-center md:text-5xl lg:text-7xl font-bold mt-5'>
                     Close more deals
                     with ROI-focused
                     lead acquisition
                 </h1>
-                <p className='flex justify-center text-center  mt-9'>
-                Belkins combines a result-oriented approach and industry-proven expertise to help you build a consistent and predictable sales channel.
+                <p className='mt-5 md:mt-9 text-center text-lg'>
+                    Belkins combines a result-oriented approach and industry-proven expertise to help you build a consistent and predictable sales channel.
                 </p>
-                <div className='flex justify-center text-center  mt-6'>
-                <button className='bg-[#FF6600] rounded-lg w-[150px] h-14 text-white hover:bg-[#773d17] font-semibold '>
-                Get a Quote
-                </button>  
+                <div className='mt-6 flex border justify-center'>
+                    <button className='bg-[#FF6600] rounded-lg w-full md:w-[150px] h-12 md:h-14 text-white hover:bg-[#773d17] font-semibold'>
+                        Get a Quote
+                    </button>
                 </div>
             </div>
-            <div className=' w-[20%] h-[100%]'>
-                <img className='object-fill' src={Popover} alt="" />
+            <div className='w-full md:w-[20%] flex justify-center md:justify-end'>
+                <img className='object-fill w-[150px] md:w-auto' src={Popover} alt="" />
             </div>
         </div>
-    )
+    );
 }
 
-export default Hero
+export default Hero;
--- src/Components/Pages/HomePage/Navbar.jsx
+++ src/Components/Pages/HomePage/Navbar.jsx
@@ -5,53 +5,49 @@ const Navbar = () => {
 
   return (
     <>
-      <div className='w-full bg-white h-[80px] top-0 flex justify-around border-b-2 sticky'>
-        <div id='Logo' className='border border-white w-[20%] flex justify-center items-center'>
-          <img src="https://belkins.io/assets/belkins-logo-light-UwcyCLNU.svg" alt="" className='w-[100px] h-auto sm:w-[150px] sm:h-[30px] lg:w-[200px] lg:h-[40px]' />
+      <div className="w-full bg-white h-[80px] top-0 flex justify-between items-center border-b-2 sticky">
+        <div id="Logo" className="w-[20%] flex justify-center items-center">
+          <img src="https://belkins.io/assets/belkins-logo-light-UwcyCLNU.svg" alt="" className="w-[80px] h-auto sm:w-[100px] sm:h-auto md:w-[150px] md:h-auto lg:w-[200px] lg:h-[50px]" />
         </div>
-        <div className='hidden lg:block lg:border border-white content-center lg:w-[45%]'>
-          <ul className='flex justify-between w-[100%]'>
-
-            
-            <li className="dropdown-container  flex items-center text-black hover:text-[#FF6600] cursor-pointer text-[15px]">   
-                What we do
-                <span><IoMdArrowDropdown /> </span>
-              <ul className="dropdown-menu w-48 bg-white shadow-lg rounded-md">
-                <li className="px-4 py-2 hover:bg-gray-200">Service 1 </li>
-                <li className="px-4 py-2 hover:bg-gray-200">Service 2 </li>
-                <li className="px-4 py-2 hover:bg-gray-200">Service 3 </li>
+        <div className="hidden lg:flex lg:w-[45%] justify-center">
+          <ul className="flex justify-between w-full">
+            <li className="relative flex items-center text-black hover:text-[#FF6600] cursor-pointer text-[12px] sm:text-[14px] md:text-[15px] group">
+              What we do
+              <span><IoMdArrowDropdown /></span>
+              <ul className="absolute hidden group-hover:block top-full left-0 w-48 bg-white shadow-lg rounded-md">
+                <li className="px-4 py-2 hover:bg-gray-200">Service 1</li>
+                <li className="px-4 py-2 hover:bg-gray-200">Service 2</li>
+                <li className="px-4 py-2 hover:bg-gray-200">Service 3</li>
               </ul>
-              </li>
-
-              <li className='flex items-center  hover:text-[#FF6600] hover:cursor-pointer text-[15px]'>
-                Solutions
-                <span><IoMdArrowDropdown /> </span></li>
-              <li className='flex items-center  hover:text-[#FF6600] hover:cursor-pointer text-[15px]'>
-                Customer Success
-                <span><IoMdArrowDropdown /> </span></li>
-              <li className='flex items-center  hover:text-[#FF6600] hover:cursor-pointer text-[15px]'>
-                Learn
-                <span><IoMdArrowDropdown /> </span></li>
-              <li className='flex items-center  hover:text-[#FF6600] hover:cursor-pointer text-[15px]'>
-                Company
-                <span><IoMdArrowDropdown /> </span></li>
-              <li className='flex items-center  hover:text-[#FF6600] hover:cursor-pointer text-[15px]'>
-                Pricing
-                <span><IoMdArrowDropdown /> </span></li>
+            </li>
+            <li className="flex items-center hover:text-[#FF6600] cursor-pointer text-[12px] sm:text-[14px] md:text-[15px]">Solutions<span><IoMdArrowDropdown /></span></li>
+            <li className="flex items-center hover:text-[#FF6600] cursor-pointer text-[12px] sm:text-[14px] md:text-[15px]">Customer Success<span><IoMdArrowDropdown /></span></li>
+            <li className="flex items-center hover:text-[#FF6600] cursor-pointer text-[12px] sm:text-[14px] md:text-[15px]">Learn<span><IoMdArrowDropdown /></span></li>
+            <li className="flex items-center hover:text-[#FF6600] cursor-pointer text-[12px] sm:text-[14px] md:text-[15px]">Company<span><IoMdArrowDropdown /></span></li>
+            <li className="flex items-center hover:text-[#FF6600] cursor-pointer text-[12px] sm:text-[14px] md:text-[15px]">Pricing<span><IoMdArrowDropdown /></span></li>
           </ul>
         </div>
-        <div className='w-[20%] flex  items-center justify-around '>
-          <div className='flex justify-center items-center w-[50%] ' >
-            {/* <img src="https://script.viserlab.com/realvest/assets/images/language/65f7c1a85f8f11710735784.png" alt="" className='h-6 w-6'/>
-              <p className='border border-white w-[90px] flex items-center'>
-                English <span><IoMdArrowDropdown /></span>
-              </p> */}
-            <button className='bg-[#FF6600] rounded-lg w-[100px] h-10 text-white hover:bg-[#773d17] font-semibold '>
-              Book a call
-            </button>
-          </div>
-
+        <div className="w-[20%] flex items-center justify-end pr-4">
+          <button className="bg-[#FF6600] rounded-lg w-[80px] h-8 text-white hover:bg-[#773d17] font-semibold text-[10px] sm:w-[100px] sm:h-10 sm:text-[12px] md:w-[120px] md:h-12 md:text-[14px] lg:w-[100px] lg:h-10 lg:text-[15px]">Book a call</button>
         </div>
+        {/* Mobile Menu Button */}
+        <div className="lg:hidden flex items-center">
+          <button className="mobile-menu-button">
+            {/* Use an icon for the mobile menu button */}
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      <div className="lg:hidden">
+        <ul className="flex flex-col items-center w-full bg-white shadow-lg rounded-md">
+          <li className="px-4 py-2 hover:bg-gray-200">What we do</li>
+          <li className="px-4 py-2 hover:bg-gray-200">Solutions</li>
+          <li className="px-4 py-2 hover:bg-gray-200">Customer Success</li>
+          <li className="px-4 py-2 hover:bg-gray-200">Learn</li>
+          <li className="px-4 py-2 hover:bg-gray-200">Company</li>
+          <li className="px-4 py-2 hover:bg-gray-200">Pricing</li>
+        </ul>
       </div>
     </>
   )
